c29697afc67183d0c2d2ea8168ba17f7a6486563
juliolucio <contact@juliolucio.com>
2016-05-18 16:38:33 -0300

    Adding to solve crazy stupid bug

---
+++ src/KafkaClosedMachine.cpp
@@ -0,0 +1,31 @@
+//
+//  MusicianComposition.cpp
+//  KafkaMachine
+//
+//  Created by Julio Lucio on 10/24/15.
+//
+//
+#include "KafkaClosedMachine.h"
+//--------------------------------------------------------------
+KafkaClosedMachine::KafkaClosedMachine(){
+}
+//--------------------------------------------------------------
+KafkaClosedMachine::~KafkaClosedMachine(){
+    delete machine;
+    delete machineController;
+}
+//--------------------------------------------------------------
+void KafkaClosedMachine::setup( string machineName , string machineFileName  , int theNumVideos ){
+    machine  = new KafkaStatesMachine( machineName );
+    machine->loadFromTSV( machineFileName );
+    machineController = new KafkaStatesMachineController( machine , machineFileName  , theNumVideos );
+}
+//--------------------------------------------------------------
+void KafkaClosedMachine::update(){
+    machineController->update();
+}
+//--------------------------------------------------------------
+void KafkaClosedMachine::draw(){
+    machineController->drawView();
+}
+
---
+++ src/KafkaClosedMachine.h
@@ -0,0 +1,28 @@
+//
+//  KafkaEdit.h
+//  KafkaEdit
+//
+//  Created by Julio Lucio on 10/April/2016.
+//
+//
+#pragma once
+
+#include "ofMain.h"
+#include "KafkaStatesMachine.h"
+#include "KafkaStatesMachineController.h"
+
+class KafkaClosedMachine{
+    
+public:
+    KafkaClosedMachine();
+    ~KafkaClosedMachine();
+    
+    void setup( string machineName , string machineFileName , int theNumVideos );
+    void update( );
+    void draw();
+    
+    //machines
+    KafkaStatesMachine* machine;
+    KafkaStatesMachineController* machineController;
+};
+
